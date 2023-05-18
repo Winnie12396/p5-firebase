41963a4ccc7d808ec896163b2ba89a22afbbad00
Winnie12396 <71814998+Winnie12396@users.noreply.github.com>
2023-05-18 11:15:05 +0800

    Update switch.js

--- switch.js
+++ switch.js
@@ -20,6 +20,25 @@ const firebaseConfig = {
       data1: value,
     });
   }
+
+  function writeNewPost(userId, val) {
+    const db = getDatabase();
+
+    // A post entry.
+    const postData = {
+        data1: val
+    };
+
+    // Get a key for a new Post.
+    const newPostKey = push(child(ref(db), 'posts')).key;
+
+    // Write the new post's data simultaneously in the posts list and the user's post list.
+    const updates = {};
+    updates['/posts/' + newPostKey] = postData;
+    updates['/user-posts/' + uid + '/' + newPostKey] = postData;
+
+    return update(ref(db), updates);
+  }
   
   var val = true;
   function setup() {
@@ -37,5 +56,6 @@ const firebaseConfig = {
   function change() {
     if (val)val = false;
     else val = true;
-    writeUserData('/', val);
+    //writeUserData('/', val);
+    writeNewPost("/", val);
   }
